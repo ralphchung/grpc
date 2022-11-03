7612cb07b7f11d16661c597cd0042a4fd75f15c8
Cheng-Yu Chung <chengyuc@google.com>
2022-11-03 13:06:32 -0400

    Remove `include/grpc/impl/codegen/byte_buffer_reader.h`

--- include/grpc/impl/codegen/byte_buffer_reader.h
+++ include/grpc/impl/codegen/byte_buffer_reader.h
@@ -21,24 +21,7 @@
 
 // IWYU pragma: private, include <grpc/byte_buffer_reader.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-struct grpc_byte_buffer;
-
-struct grpc_byte_buffer_reader {
-  struct grpc_byte_buffer* buffer_in;
-  struct grpc_byte_buffer* buffer_out;
-  /** Different current objects correspond to different types of byte buffers */
-  union grpc_byte_buffer_reader_current {
-    /** Index into a slice buffer's array of slices */
-    unsigned index;
-  } current;
-};
-
-#ifdef __cplusplus
-}
-#endif
+/// TODO(chengyuc): Remove this file after solving compatibility.
+#include <grpc/byte_buffer_reader.h>
 
 #endif /* GRPC_IMPL_CODEGEN_BYTE_BUFFER_READER_H */
