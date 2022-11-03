a0742bb98cf487b76040242efd90c2207d880eab
ralphchung <ralphchung@users.noreply.github.com>
2022-11-03 19:01:35 +0000

    Automated change: Fix sanity tests

--- include/grpc/impl/codegen/fork.h
+++ include/grpc/impl/codegen/fork.h
@@ -19,9 +19,11 @@
 #ifndef GRPC_IMPL_CODEGEN_FORK_H
 #define GRPC_IMPL_CODEGEN_FORK_H
 
-// IWYU pragma: private, include <grpc/fork.h>
+// IWYU pragma: private
 
 /// TODO(chengyuc): Remove this file after solving compatibility.
+#include <grpc/support/port_platform.h>
+
 #include <grpc/fork.h>
 
 #endif /* GRPC_IMPL_CODEGEN_FORK_H */
